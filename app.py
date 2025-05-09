e70799932d6c3e0e71836da78f808df9b9b44609
ProfJuliani <rodrigo.godoy@mackenzie.br>
2025-05-09 15:58:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,9 @@
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
-
+from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/hello', methods=['POST'])
+@app.route("/")
 def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+    return "Hello, World!"
 
 if __name__ == '__main__':
    app.run()
